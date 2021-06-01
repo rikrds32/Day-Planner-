dcd9a8261daee438e3f2f80f532f79c4f9f7167d
Ricardo Salazar <no-replay>
2021-05-31 23:46:17 -0500

    add determine time function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // this is the date of the current day//
 
 var today = moment("05/28/2021");
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+// determine time function//
+function determineTime(){
+    var currentTime= parseInt(moment().format("HH"));
 
+}
