408e7e33060a0815e7fb37f5c5ba0a3d5ad9f67d
Ricardo Salazar <no-replay>
2021-06-01 00:09:57 -0500

    add id to the hours

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ var today = moment("05/28/2021");
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 // determine time function//
 function determineTime(){
-    var currentTime= parseInt(moment().format("HH"));
-
+    var currentTime = parseInt(moment().format("HH"));
+    var timeBlock = 8;
+    for (time = 8; time >= 17; time++) {
+        if (timeBlock < currentTime){
+            
+        }
+    }
 }
